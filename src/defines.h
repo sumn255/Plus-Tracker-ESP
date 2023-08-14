f0198466564836c5f18edc41001962b6dc67df8c
sumn255 <sumn255@gmail.com>
2023-08-15 00:39:42 +0800

    c3 ver initial commit

--- src/defines.h
+++ src/defines.h
@@ -26,11 +26,11 @@
 // ================================================
 
 // Set parameters of IMU and board used
-#define IMU IMU_BNO085
+#define IMU IMU_ICM20948
 #define SECOND_IMU IMU
-#define BOARD BOARD_SLIMEVR
+#define BOARD BOARD_LOLIN_C3_MINI
 #define IMU_ROTATION DEG_270
-#define SECOND_IMU_ROTATION DEG_270
+//#define SECOND_IMU_ROTATION DEG_270
 
 // Battery monitoring options (comment to disable):
 //   BAT_EXTERNAL for ADC pin, 
